90f13adcad5b668ae8226187ec7e55ee1a97850a
bigless27 <lessonryan@gmail.com>
2018-10-21 16:53:36 -0500

    add user signup

--- src/models/User.js
+++ src/models/User.js
@@ -1,18 +1,24 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import uniqueValidator from 'mongoose-unique-validator';
 
 // Todo: add uniqueness and email valifation to email field
 const schema = new mongoose.Schema({
-    email: { type: String, required: true, lowercase: true, index: true},
-    passwordHash: { type: String, required: true }
+    email: { type: String, required: true, lowercase: true, index: true, unique: true },
+    passwordHash: { type: String, required: true },
+    confirmed: { type: Boolean, dafault: false }
 }, { timestamps: true });
 
 schema.methods.isValidPassword = function isValidPassword(password) {
     return bcrypt.compareSync(password, this.passwordHash);
 }
 
-schema.methods.generateJWT = function  generateJWT() {
+schema.methods.setPassword = function setPassword(password) {
+    this.passwordHash = bcrypt.hashSync(password, 10);
+}
+
+schema.methods.generateJWT = function generateJWT() {
     return jwt.sign({
         email: this.email
     }, process.env.JWT_SECRET);
@@ -21,8 +27,11 @@ schema.methods.generateJWT = function  generateJWT() {
 schema.methods.toAuthJSON = function toAuthJSON() {
     return {
         email: this.email,
+        confirmed: this.confirmed,
         token: this.generateJWT()
     }
 }
+
+schema.plugin(uniqueValidator, { message: "This email is already taken." })
 
 export default mongoose.model('User', schema);
