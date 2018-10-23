485af8b6a2c4acc9dcf3345d18843189e2c5231c
bigless27 <lessonryan@gmail.com>
2018-10-23 09:51:13 -0500

    add initial email set up

--- src/models/User.js
+++ src/models/User.js
@@ -7,7 +7,8 @@ import uniqueValidator from 'mongoose-unique-validator';
 const schema = new mongoose.Schema({
     email: { type: String, required: true, lowercase: true, index: true, unique: true },
     passwordHash: { type: String, required: true },
-    confirmed: { type: Boolean, dafault: false }
+    confirmed: { type: Boolean, dafault: false },
+    confirmationToken: { tupe: String, default: '' }
 }, { timestamps: true });
 
 schema.methods.isValidPassword = function isValidPassword(password) {
@@ -16,6 +17,14 @@ schema.methods.isValidPassword = function isValidPassword(password) {
 
 schema.methods.setPassword = function setPassword(password) {
     this.passwordHash = bcrypt.hashSync(password, 10);
+}
+
+schema.methods.setConfirmationToken = function setConfirmationToken() {
+    this.confirmationToken = this.generateJWT();
+}
+
+schema.methods.generateConfirmationUrl = function generateConfirmationUrl() {
+    return `${process.env.HOST}/confirmation/${this.confirmationToken}`
 }
 
 schema.methods.generateJWT = function generateJWT() {
